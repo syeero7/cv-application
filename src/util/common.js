98008a279714e195947466e6a5755c20fb7b6f2e
syeero7 <EmalHasitha@proton.me>
2024-12-09 14:02:02 +0530

    chore: remove unused hasOnlyFirstChild function

--- src/util/common.js
+++ src/util/common.js
@@ -2,11 +2,6 @@ export function getInputValue(form, dataset) {
   return form.querySelector(`[data-${dataset}]`).value.trim();
 }
 
-export function hasOnlyFirstChild(parentElementSelector) {
-  const parentElement = document.querySelector(parentElementSelector);
-  return parentElement?.children.length === 1;
-}
-
 export function getCurrentYear() {
   return new Date().toISOString().slice(0, 4);
 }
