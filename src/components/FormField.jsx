5baca2b675263fd0bbb00b1d9c7d06fe416bc2a2
Emal <EmalHasitha@proton.me>
2025-05-19 03:50:29 +0530

    feat: add FormField component

---
+++ src/components/FormField.jsx
@@ -0,0 +1,30 @@
+function FormField({ label, name, value, type = "text" }) {
+  const isYearInput = type === "number" && (name === "start" || name === "end");
+  let currentYear;
+
+  if (isYearInput) {
+    const date = new Date();
+    currentYear = date.getFullYear();
+  }
+
+  return (
+    <label>
+      <span>{label}</span>
+      <input
+        required
+        type={type}
+        name={name}
+        defaultValue={isYearInput ? value || currentYear : value}
+        max={isYearInput ? currentYear : undefined}
+        min={isYearInput ? 1900 : undefined}
+        onChange={
+          type === "tel"
+            ? (e) => e.target.value.replace(/[^0-9]/g, "")
+            : undefined
+        }
+      />
+    </label>
+  );
+}
+
+export default FormField;
